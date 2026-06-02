611d0b95340491e809eb9422ed27ad2035e75986
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-06-02 13:22:08 +0530

    Add PostgreSQL table for storing news analytics

--- test_db.py
+++ test_db.py
@@ -11,3 +11,18 @@ print("Database connected successfully!")
 
 
 cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS news_data (
+    id SERIAL PRIMARY KEY,
+    news_date DATE,
+    source_name VARCHAR(255),
+    title TEXT,
+    sentiment_score FLOAT,
+    sentiment_label VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+""")
+
+# Save changes
+conn.commit()
