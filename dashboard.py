013075c76c21132123873e5249ce7e00108577b6
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-06-02 12:53:57 +0530

    Set dashboard title and wide layout

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,8 @@
 import streamlit as st
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine 
+
+st.set_page_config(
+    page_title="News Analytics Dashboard",
+    layout="wide"
+)
