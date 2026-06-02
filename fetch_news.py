65a42bd886833e805884ee6ffdd44959078e31f6
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-06-02 13:13:15 +0530

    Configure News API and PostgreSQL RDS connection

--- fetch_news.py
+++ fetch_news.py
@@ -2,3 +2,13 @@ import requests
 import pg8000
 from textblob import TextBlob
 from datetime import datetime
+
+API_KEY = "53571c2a4caf4ab4ae1c27abc5fd2153"
+
+conn = pg8000.connect(
+    host="database-1.cvkg622c6us1.ap-southeast-2.rds.amazonaws.com",
+    database="news_db",
+    user="postgres",
+    password="saniyabijoy",
+    port="5432"
+)
