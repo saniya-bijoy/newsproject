6ca21e68d2a970790d60bf2ba81befa3e73aef2e
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-06-02 13:20:47 +0530

    Configure PostgreSQL RDS connectivity"

--- test_db.py
+++ test_db.py
@@ -1,1 +1,13 @@
 import psycopg2
+conn = psycopg2.connect(
+    host="database-1.cvkg622c6us1.ap-southeast-2.rds.amazonaws.com",
+    database="news_db",
+    user="postgres",
+    password="saniyabijoy",
+    port="5432"
+)
+
+print("Database connected successfully!")
+
+
+cur = conn.cursor()
