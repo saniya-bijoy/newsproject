ea266d03e04e678bff10dd69b64e3744c416c712
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-06-02 12:57:54 +0530

    Improve dashboard layout and visual design

--- dashboard.py
+++ dashboard.py
@@ -13,3 +13,22 @@ engine = create_engine(DATABASE_URL)
 
 df = pd.read_sql("SELECT * FROM news_data ORDER BY created_at DESC", engine)
 
+
+st.markdown("""
+<style>
+body {
+    background-color: #0e1117;
+}
+.main-title {
+    font-size: 42px;
+    font-weight: bold;
+    color: white;
+    text-align: center;
+    margin-bottom: 30px;
+}
+.sidebar-text {
+    font-size: 15px;
+    line-height: 1.6;
+}
+</style>
+""", unsafe_allow_html=True)
