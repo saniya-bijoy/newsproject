8621718a97db60b1b65c55731b0cb35e98e0b07a
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-06-04 11:20:25 +0530

    Added config file module

---
+++ Lambda_Modules/config.py
@@ -0,0 +1,11 @@
+# DATABASE CONFIGURATION
+
+DB_HOST = "database-1.cvkg622c6us1.ap-southeast-2.rds.amazonaws.com"
+DB_NAME = "news_db"
+DB_USER = "saniyabijoy"
+DB_PASSWORD = "postgres"
+DB_PORT = 5432
+
+# NEWS API
+
+NEWS_API_KEY = "53571c2a4caf4ab4ae1c27abc5fd2153"
